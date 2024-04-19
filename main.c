2bf7811b959812eff5c4283ebddca9ca88977112
Poonyawatt  Klumnaim <poon@pnwttklm.com>
2024-04-19 17:56:06 +0700

    some

--- main.c
+++ main.c
@@ -1,15 +1,70 @@
 #include <stdio.h>
 #include <string.h>
-#define B 300;
-#define R 100;
+#include <pthread.h>
+#define B 300
+#define R 100
 
-int main(){
-	char word[B];
-	while(1){
-		if(scanf(" %s", word) != EOF){
-			
-			printf("word: %s\n", word);
-}else{break;}
+char storage[R][B];
+int indexStorageIn = 0;
+char word[R][B];
+int indexWordIn = 0;
+int indexWordOut = 0;
+
+void *readFile(void *arg)
+{
+	char temp[B];
+	while (1)
+	{
+		if (scanf("%s", temp) != EOF)
+		{
+			strcpy(word[indexWordIn], temp);
+			indexWordIn = (indexWordIn + 1) % R;
+		}
+		else
+		{
+			break;
+		}
+	}
 }
-return 0;
+void getWord() {
+	char temp[B];
+	while (1)
+	{
+		if (scanf("%s", temp) != EOF)
+		{
+			strcpy(word[indexStorageIn], temp);
+			indexStorageIn = (indexStorageIn + 1) % R;
+		}
+		else
+		{
+			break;
+		}
+	}
+}
+void *spellCheck(void *arg)
+{
+	while (1)
+	{
+		if (indexWordOut != indexWordIn)
+		{
+			word[indexWordOut];
+			indexWordOut = (indexWordOut + 1) % R;
+		}else{
+			break;
+		}
+	}
+}
+void counter() {}
+void writer() {}
+
+int main()
+{
+	pthread_t pid;
+	pthread_create(&pid, NULL, readFile, NULL);
+	pthread_join(pid, NULL);
+	for (int i = 0; i < indexWordIn; i++)
+	{
+		printf("%s\n", word[i]);
+	}
+	return 0;
 }
